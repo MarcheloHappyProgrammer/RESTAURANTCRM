6a8b93e79a0f4409e192513ce15d57fd7a6e296b
Marchelo <jfmarchelo29@hotmail.com>
2024-10-28 00:21:38 -0500

    Descripción de los cambios realizados

--- src/main/java/com/restaurant/management/repository/DishScheduleOrganitationRepository.java
+++ src/main/java/com/restaurant/management/repository/DishScheduleOrganitationRepository.java
@@ -5,11 +5,8 @@ import java.util.Date;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-
-import com.restaurant.management.model.Dish;
 import com.restaurant.management.model.DishSchedule;
 @Repository
 public interface DishScheduleOrganitationRepository extends PagingAndSortingRepository<DishSchedule, Long>{
